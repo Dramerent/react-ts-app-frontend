2d79b494c1e9c42a6a755a6b106f7dbcebe58c1c
Dramerent <gallyamovilha@yandex.ru>
2026-03-18 23:02:35 +0500

    v0.13

--- src/useContext/useContext.tsx
+++ src/useContext/useContext.tsx
@@ -7,7 +7,7 @@ export const ContextProvider = ({children}: {children: React.ReactNode}) =>{
     const [cardsInfo, setCardsInfo] = useState<AllInfoType[]>([])
 
     const getAllInfo = async() => {
-        const gettedInfo = await axios.get<AllInfoType[]>("https://react-ts-app-backend.onrender.com/app/allInfo", {withCredentials: true})
+        const gettedInfo = await axios.get<AllInfoType[]>("https://api.render.com/deploy/srv-d6skug7fte5s73fa8e90?key=UjFDlEwedOI/app/allInfo", {withCredentials: true})
         .then((el) => el.data)
         setCardsInfo(gettedInfo)
     }
